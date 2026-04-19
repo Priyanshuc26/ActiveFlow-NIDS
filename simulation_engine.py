ab5783804f494bd2c787f8cea787917e76e48b4f
Priyanshuc26 <priyanshuc2606@gmail.com>
2026-04-20 04:38:17 +0530

    Constants inside Simulation Engine updated

--- simulation_engine.py
+++ simulation_engine.py
@@ -5,15 +5,16 @@ import requests
 
 from IDS_Pipeline.logging.logger import logging
 from IDS_Pipeline.exception.exception import CustomException
+from IDS_Pipeline.constant.training_pipeline import SIMULATION_FILE_PATH,API_POST_REQ_IP
 
 try:
-    df = pd.read_csv('./pcap_processed_csv/Friday-WorkingHours.pcap_lycos.csv') 
+    df = pd.read_csv(SIMULATION_FILE_PATH) 
     df.drop(columns=['flow_id','label'],inplace=True)
     df = df.iloc[350000:]
     
     for rows in df.itertuples(index=False):
-        packets = rows._asdict()
-        requests.post(url='http://192.168.29.83:8000/predict',json=packets)
+        packets = rows._asdict()      # NamedTuple has _asdict() method to convert the dta into a dictionary
+        requests.post(url=API_POST_REQ_IP,json=packets)
         # print(packets,'\n')
         time.sleep(0.001)
 except Exception as e:
